90ddd61c4fd052ec3c266db471b12a3f19aaa598
endless01 <zze@DESKTOP-SB6L4RD>
2025-08-02 16:48:27 +0800

    fix error code

--- src/Pipelines.Core/Configuration/StepConfiguration.cs
+++ src/Pipelines.Core/Configuration/StepConfiguration.cs
@@ -1,5 +1,4 @@
 ﻿namespace Pipelines.Core.Configuration;
-namespace Pipelines.Core.Configuration;
 
 public class StepConfiguration
 {
--- src/Pipelines/Program.cs
+++ src/Pipelines/Program.cs
@@ -44,7 +44,6 @@ builder.Services.AddCors(options =>
     });
 });
 
-
 var app = builder.Build();
 
 if (app.Environment.IsDevelopment())
