70316436b7ff6f1ed0486eaddbb8da1f03bfb507
endless01 <zze@DESKTOP-SB6L4RD>
2025-08-02 13:58:47 +0800

    Remove unused validation API and error handling classes to streamline the codebase.

--- src/Pipelines/Apis/ValidateApi.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Pipelines.Apis;
-public static class ValidateApi
-{ 
-    public static RouteGroupBuilder MapValidateApiV1(this IEndpointRouteBuilder app)
-    {
-        var api = app.MapGroup("api/validate");
-        return api;
-    }
-}
